b930f43af3e1cb8c238fc3fe16b6165ab23e684e
NEOZEO64 <neozeo64@pm.me>
2023-12-29 14:30:15 +0100

    added client code

--- client.py
+++ client.py
@@ -2,51 +2,62 @@ import pygame
 from pygame.locals import *
 import gameUtils
 
-
-# Pygame starten
 pygame.init()
+windowSurface = pygame.display.set_mode((500, 500)) # create window
 
-# Fenster erstellen
-windowSurface = pygame.display.set_mode((1024, 600), pygame.FULLSCREEN)
+lemonPic = gameUtils.loadIMG("./Lemon.png", 128) # width 128
 
-# Bild laden (load image) mit Breite 60
-shipPic = gameUtils.loadIMG("Res/body_03.png", 60)
-
-
-
-shipWidth = shipPic.get_width()
-shipHeight = shipPic.get_height()
-print(shipHeight)
-
-
-# Farben in Variablen speichern
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-# Hintergrund weiß zeichnen
-windowSurface.fill(WHITE)
 
-# Roten Kreis zeichen (Position: X=300, Y=50 mit Radius 20)
-pygame.draw.circle(windowSurface, RED, (0, 0), 500, 0)
+class Player:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def show(self):
+        # draw lemon!
+        windowSurface.blit(lemonPic, (self.x,self.y))
 
-# Blauen Kreis zeichen (Position: X=300, Y=50 mit Radius 20)
-pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
 
-# Bild zeichnen! (An Position X=60, Y=200)
-windowSurface.blit(shipPic, (60,200))
+players = []
 
-# Alles gezeichnete tatsächlich anzeigen
-pygame.display.update()
+players.append(Player(50,50))
 
-run = True
+
+
+def render():
+    windowSurface.fill(RED)
+
+    for i in range(len(players)):
+        players[i].show()
+
+    # put buffer to screen
+    pygame.display.update()
+
+
 
 # Spiel-Schleife
+run = True
+
 while run:
     # Informationen (Events) über das Pygame-Fenster abfragen
     for event in pygame.event.get():
         # Wenn die Information gerade ein beenden-Event ist:
         if event.type == QUIT:
             run = False
+
+    keys = gameUtils.getKeys()
+    if keys["w"]:
+        players[0].y -= 1
+    if keys["a"]:
+        players[0].x -= 1
+    if keys["s"]:
+        players[0].y += 1
+    if keys["d"]:
+        players[0].x += 1
+        
+    render()
