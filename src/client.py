815284bffdb51c251d2f426610e2d3809c57dbe4
NEOZEO64 <neozeo64@pm.me>
2023-12-30 13:37:38 +0100

    added multithreaded multiplayer

--- src/client.py
+++ src/client.py
@@ -1,8 +1,8 @@
 # Python client
 # written on 29.12.2023 on Chaos Communication Congress 37c3
-import sys, socket, pygame, gameUtils, random
+import sys, socket, pygame, gameUtils, random, threading
 
-# try connecting to server, otherwise quit
+# get hostname and port from command line
 if len(sys.argv) <= 2 or sys.argv[1] == "--help":
     print("Syntax: python client.py <server ip> <port>")
     exit()
@@ -20,23 +20,46 @@ playerHeight = 64
 x = random.randrange(0, width-playerWidth)  # start position
 y = random.randrange(0, height-playerHeight)
 
-# connect to server with specified hostname
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    client.connect((hostname, port))
-    print("Start socket")
-    client.send(str.encode("{},{}".format(x, y)))
-    print("Message on join:", client.recv(2048).decode())
-except: # if no connection just quit
-    print("No connection")
-    sys.exit()
-print("Joined server")
-
 # create pygame window
 win = pygame.display.set_mode((width, height)) # width / height
 lemonPic = gameUtils.loadIMG("../img/Lemon.png", playerWidth, playerHeight)
 clock = pygame.time.Clock()
+otherPlayers = [] # contains tuples (<x>,<y>) of other player positons
 run = True
+
+
+def getOtherPlayers():
+    global otherPlayers, run
+
+    # connect to server with specified hostname
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        client.connect((hostname, port))
+        client.send(str.encode("{},{}".format(x, y)))
+        print("Message on join:", client.recv(2048).decode())
+    except: # if no connection just quit
+        print("Connection refused")
+        sys.exit()
+    print("Joined server")
+
+    while run:
+        try:
+            otherPlayers = []
+            client.send(str.encode("{},{}".format(x, y))) # send client position...
+            msg = client.recv(2048).decode() # to get server response
+            
+            if msg != "you are alone": # if there are players
+                coordsTup = msg[:-1].split(";") # then split player coordinates
+                if coordsTup != ['']: 
+                    for tup in coordsTup:
+                        tup = tup.split(",")
+                        otherPlayers.append((int(tup[0]), int(tup[1])))
+        except socket.error as e:
+            print(e)
+            run = False
+
+otherPlayerThread = threading.Thread(target=getOtherPlayers)
+otherPlayerThread.start()
 
 while run:
     # move player client on key presses
@@ -55,25 +78,14 @@ while run:
     if x < -playerWidth or x > width or y < -playerHeight or y > height:
         run = False
 
+    # RENDER
     win.fill((255,100,100)) # draw background white
+    for i in range(len(otherPlayers)):
+        win.blit(lemonPic, otherPlayers[i]) # draw other players
     win.blit(lemonPic, (x,y)) # draw player
-
-    # managing other players
-    try:
-        client.send(str.encode("{},{}".format(x, y))) # send client position...
-        msg = client.recv(2048).decode() # to get server response
-        
-        if msg != "no!": # if there are players
-            coordsTup = msg[:-1].split(";") # then split player coordinates
-            if coordsTup != ['']: 
-                for tup in coordsTup:
-                    print(tup)
-                    tup = tup.split(",")
-                    win.blit(lemonPic, (int(tup[0]), int(tup[1])))
-    except socket.error as e:
-        print(e)
 
     pygame.display.update()
     clock.tick(fps) # for 30 fps
 
 pygame.quit()
+sys.exit()
