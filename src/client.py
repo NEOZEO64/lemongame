0eb889e013d5b8213727191bf9701140d5870a51
NEOZEO64 <neozeo64@pm.me>
2023-12-30 14:17:20 +0100

    ADDED EXPLOSIONS

--- src/client.py
+++ src/client.py
@@ -32,6 +32,44 @@ clock = pygame.time.Clock()
 otherPlayers = [] # contains tuples (<x>,<y>) of other player positons
 run = True
 
+class Explosion:
+    ws = [ # widths
+        80 # type 0
+    ]
+    
+    animations = []
+
+    tempAnimation = []
+    for i in range(1,9):
+        tempAnimation.append(gameUtils.loadIMG("../img/Explosion_{}.png".format(i), ws[0]))
+    animations.append(tempAnimation)
+
+    numbers = [len(animation) for animation in animations] # animation pic length
+
+    hs = [i[0].get_height() for i in animations]# heights
+
+    objs = []
+
+    def __init__(self,x,y, type):
+        self.cx = x # center
+        self.cy = y
+        self.type = type
+        self.x = self.cx-Explosion.ws[self.type]/2
+        self.y = self.cy-Explosion.hs[self.type]/2
+        self.speed = 0.5
+        self.state = 0
+        
+    def move(self): # returns if animation done
+        self.state += self.speed
+
+        if self.state >= Explosion.numbers[self.type]:
+            return True
+        return False
+        
+    def show(self):
+        win.blit(self.animations[self.type][int(self.state)], (self.x, self.y))
+        #window.blit(self.animations[self.type][int(self.state)], (self.x, self.y))
+
 
 def getOtherPlayers():
     global otherPlayers, run
@@ -53,13 +91,17 @@ def getOtherPlayers():
         try: 
             client.send(str.encode("{},{}".format(int(x), int(y)))) # send client position...
             msg = client.recv(2048).decode() # to get server response
-            otherPlayers = []
+
+            nowPlayers = []
             if msg != "you are alone": # if there are players
                 coordsTup = msg[:-1].split(";") # then split player coordinates
                 if coordsTup != ['']: 
                     for tup in coordsTup:
                         tup = tup.split(",")
-                        otherPlayers.append((int(tup[0]), int(tup[1])))
+                        nowPlayers.append((int(tup[0]), int(tup[1])))
+            for i in range(len(nowPlayers)-len(otherPlayers)):
+                Explosion.objs.append(Explosion(nowPlayers[i][0]+playerWidth/2,nowPlayers[i][1]+playerHeight/2,0)) # 0 for type
+            otherPlayers = nowPlayers
         except socket.error as e:
             print(e)
             run = False
@@ -86,15 +128,28 @@ while run:
     x += velX
     y += velY
 
+    
+
     # quit game by move out of window
     if x < -playerWidth or x > width or y < -playerHeight or y > height:
         run = False
 
+    # handle explosions
+    i = 0
+    while i < len(Explosion.objs):
+        if Explosion.objs[i].move() == True:
+            del Explosion.objs[i]
+        else:
+            i += 1
+
     # RENDER
-    win.fill((255,100,100)) # draw background white
+    win.fill((140,100,255)) # draw background white
     for i in range(len(otherPlayers)):
         win.blit(lemonPic, otherPlayers[i]) # draw other players
     win.blit(lemonPic, (x,y)) # draw player
+
+    for e in Explosion.objs:
+        e.show()
 
     pygame.display.update()
     clock.tick(fps) # for 30 fps
