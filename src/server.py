537698be8c190b7fa9e86bfab6e7d881b72f4808
tippfehlr <tippfehlr@tippfehlr.eu>
2023-12-30 13:14:00 +0100

    working server - dead players

--- src/server.py
+++ src/server.py
@@ -23,40 +23,36 @@ s.listen(2)
 print("Waiting for a connection, Server Started")
 
 # player_positions[player_id] = (X, Y)
-player_positions = [(0, 0), (0, 0), (0, 0)]
+player_positions = []
 
 def threaded_client(conn, player):
-    # conn.send(str.encode(make_pos(player_positions[player])))
     global player_positions
     
     while True:
-        # try:
         raw = conn.recv(2048).decode()
-        print("[Client {}]: Received: {}".format(player, raw))
-        data = raw.split(",")
+        
+        if raw:
+            print("[Client {}]: Received: {}".format(player, raw))
+            data = raw.split(",")
 
-        # if len(player_positions) < player - 1:
-        #     player_positions.append((1, 1))
+            while len(player_positions) <= player:
+                player_positions.append(None)
         
-        player_positions[player] = (int(data[0]), int(data[1]))
-        print("[Client {}]: Position: X{} Y{}".format(player, player_positions[player][0], player_positions[player][1]))
+            player_positions[player] = (int(data[0]), int(data[1]))
+            print("[Client {}]: Position: X{} Y{}".format(player, player_positions[player][0], player_positions[player][1]))
         
-        if not data:
-            conn.sendall(str.encode("no!"))
-            break
-        else:
             reply = ""
             for (i, pos) in enumerate(player_positions):
                 if not i == player:
                     reply += "{},{};".format(pos[0], pos[1])
-            print("[Client {}]: Sending : ", player, reply)
-            conn.sendall(str.encode(reply))
-        # except:
-        #     print("exception!!!")
-        #     break
+            print("[Client {}]: Sending: {}".format(player, reply))
+            if reply == "":
+                conn.sendall(str.encode("no!"))
+            else:
+                conn.sendall(str.encode(reply))
     global player_count
     player_count -= 1
-    player_positions.pop(player)
+    player_positions[player] = None
     print("Lost connection, {} remaining player(s)".format(player_count))
     conn.close()
 
